c54f227fe2eaf6b882f3ac5196c3a16ca51e37de
Anton Trantin <anton@trantin.ru>
2016-09-17 20:55:17 +0300

    Add ignore

---
+++ YorsoGettingXbox/YorsoGettingXbox/Models/SignerEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace YorsoGettingXbox.Models
+{
+    public class SignerEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PrivateKey { get; set; }
+    }
+}
