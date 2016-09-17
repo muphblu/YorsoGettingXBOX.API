8ab58e278d2cfe81a5dab0f27612bfff3cb73260
Anton Trantin <anton@trantin.ru>
2016-09-17 21:09:31 +0300

    New line endings

--- YorsoGettingXbox/YorsoGettingXbox/Controllers/DealsController.cs
+++ YorsoGettingXbox/YorsoGettingXbox/Controllers/DealsController.cs
@@ -16,8 +16,8 @@ namespace YorsoGettingXbox.Controllers
         {
             var deals = new[]
             {
-                new DealEntity() {Description = "Description 1", Title = "Title 1", ID = 1},
-                new DealEntity() {Description = "Description 2", Title = "Title 2", ID = 2},
+                new DealEntity() {Description = "Description 1", Title = "Title 1", Id = 1},
+                new DealEntity() {Description = "Description 2", Title = "Title 2", Id = 2},
             };
             /*var response = Request.CreateResponse(HttpStatusCode.OK);
             response.Content = new StringContent(new JavaScriptSerializer().Serialize(deals), Encoding.UTF8, "application/json");
@@ -33,7 +33,7 @@ namespace YorsoGettingXbox.Controllers
                 new JavaScriptSerializer().Serialize(new DealEntity() { Description = "Description 1", Title = "Title 1", ID = 1 }), 
                 Encoding.UTF8, 
                 "application/json");*/
-            return new DealEntity { Description = "Description 1", Title = "Title 1", ID = 1 };
+            return new DealEntity { Description = "Description 1", Title = "Title 1", Id = 1 };
         }
 
         // GET: api/deals/1/documents
@@ -42,9 +42,9 @@ namespace YorsoGettingXbox.Controllers
         {
             var documents = new[]
             {
-                new DocumentEntity() { ID = 1 },
-                new DocumentEntity() { ID = 2 },
-                new DocumentEntity() { ID = 3 },
+                new DocumentEntity() { Id = 1 },
+                new DocumentEntity() { Id = 2 },
+                new DocumentEntity() { Id = 3 },
             };
 
             /*var response = Request.CreateResponse(HttpStatusCode.OK);
