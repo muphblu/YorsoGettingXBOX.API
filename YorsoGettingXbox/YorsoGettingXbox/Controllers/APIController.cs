da2fd022f1ce74a96133fbbb9bfad1b9e55a2795
Aidar Shaikhiev <muphblu09@gmail.com>
2016-09-17 19:36:18 +0300

    Add api controller

---
+++ YorsoGettingXbox/YorsoGettingXbox/Controllers/APIController.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace YorsoGettingXbox.Controllers
+{
+    public class APIController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage UploadDocument()
+        {
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Deals()
+        {
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Documents()
+        {
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AddDeal()
+        {
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AddDocument()
+        {
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Sign()
+        {
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Check()
+        {
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage DocInfo()
+        {
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+    }
+}
