a72a29131829ec8afa7a8d234f4981190740910d
Anton Trantin <anton@trantin.ru>
2016-09-17 18:25:08 +0300

    All this stuff

---
+++ YorsoGettingXbox/YorsoGettingXbox/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Helpers;
+using System.Web.Http;
+using System.Web.Mvc;
+using YorsoGettingXbox.Models;
+
+namespace YorsoGettingXbox.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public ActionResult Index()
+        {
+            return new JsonResult();
+        }
+    }
+}
---
+++ YorsoGettingXbox/YorsoGettingXbox/Models/DealEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace YorsoGettingXbox.Models
+{
+    public class DealEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
